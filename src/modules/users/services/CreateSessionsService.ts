08dae828b1bd9f9fddfba45b5922ed4ae3f6ad47
almakad <carls-f@live.com>
2021-03-11 20:35:05 -0300

    feat: add middleware isAuthenticated to verify authenticated users

--- src/modules/users/services/CreateSessionsService.ts
+++ src/modules/users/services/CreateSessionsService.ts
@@ -1,4 +1,6 @@
-import { compare, hash } from 'bcryptjs';
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+import authConfig from '../../../config/auth';
 import { getCustomRepository } from 'typeorm';
 import AppError from '../../../shared/errors/AppError';
 import User from '../typeorm/entities/User';
@@ -9,12 +11,13 @@ interface IRequest {
   password: string;
 }
 
-// interface IResponse {
-//   user: User;
-// }
+interface IResponse {
+  user: User;
+  token: string;
+}
 
 class CreateSessionsSerives {
-  public async execute({ email, password }: IRequest): Promise<User> {
+  public async execute({ email, password }: IRequest): Promise<IResponse> {
     const usersRepository = getCustomRepository(UsersRepository);
 
     const user = await usersRepository.findByEmail(email);
@@ -28,7 +31,14 @@ class CreateSessionsSerives {
       throw new AppError('Incorrect email/password.', 401);
     }
 
-    return user;
+    const token = sign({}, authConfig.jwt.secret, {
+      subject: user.id,
+      expiresIn: authConfig.jwt.expiresIn,
+    });
+    return {
+      user,
+      token,
+    };
   }
 }
 
---
+++ src/shared/http/middlewares/isAuthenticated.ts
@@ -0,0 +1,38 @@
+import { NextFunction, Request, Response } from 'express';
+import { verify } from 'jsonwebtoken';
+import AppError from '../../errors/AppError';
+import authConfig from '../../../config/auth';
+
+interface ITokenPayload {
+  iat: number;
+  exp: number;
+  sub: string;
+}
+
+export default function isAuthenticated(
+  req: Request,
+  res: Response,
+  _next: NextFunction,
+): void {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    throw new AppError('JWT Token is missing.');
+  }
+
+  const [, token] = authHeader.split(' ');
+
+  try {
+    const decodedToken = verify(token, authConfig.jwt.secret);
+
+    const { sub } = decodedToken as ITokenPayload;
+
+    req.user = {
+      id: sub,
+    };
+
+    return _next();
+  } catch (error) {
+    throw new AppError('Invalid JWT Token');
+  }
+}
